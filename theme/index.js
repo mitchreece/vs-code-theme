22bb807b14b2ff17627a1bbe9cc46b137dc3be35
mitchreece <mitchellreece18@gmail.com>
2018-02-05 22:28:55 +0000

    add badge styles

--- theme/index.js
+++ theme/index.js
@@ -2,6 +2,11 @@ const { colors } = require('@mitchreece/theme-variables')
 
 module.exports = {
     colors: {
+        "badge.background": colors.secondary,
+        "badge.foreground": colors.fontLight,
+        "activityBarBadge.background": colors.secondary,
+        "activityBarBadge.foreground": colors.fontLight,
+        
         "statusBar.background": colors.primary,
         "statusBar.foreground": colors.secondary,
         "statusBarItem.hoverBackground": colors.primaryDark,
